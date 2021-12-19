12da1a250d588f986b34cc3f97f7e408bd91162c
kannna5296 <kannnahacks5296@gmail.com>
2021-12-19 16:27:52 +0900

    新規ファイル追加

---
+++ src/main/resources/db/migration/V0_0_1__新規.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[user](
+[id] [int] IDENTITY(1,1) NOT NULL,
+[name] [nvarchar](50) NOT NULL,
+[password] [nvarchar](50) NOT NULL,
+[created_at] [datetime2](2) NOT NULL,
+[updated_at] [datetime2](2) NOT NULL
+)
