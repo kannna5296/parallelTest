55beb8e3d340c5132360497b68b978db074be46f
kannna5296 <kannnahacks5296@gmail.com>
2021-12-19 20:58:17 +0900

    マイグレートされない問題解決

--- src/main/resources/db/migration/V0_0_1__新規.sql
+++ src/main/resources/db/migration/V0_0_1__新規.sql
@@ -1,7 +1,18 @@
 CREATE TABLE [dbo].[app_user](
-[id] [int] IDENTITY(1,1) NOT NULL,
-[name] [nvarchar](50) NOT NULL,
-[password] [nvarchar](50) NOT NULL,
-[created_at] [datetime2](2) NOT NULL,
-[updated_at] [datetime2](2) NOT NULL
+    [id] [int] IDENTITY(1, 1) NOT NULL,
+    [name] [nvarchar](50) NOT NULL,
+    [password] [nvarchar](50) NOT NULL,
+    [created_at] [datetime2](2) NOT NULL,
+    [updated_at] [datetime2](2) NOT NULL,
+    PRIMARY KEY CLUSTERED([id] ASC) WITH(
+        PAD_INDEX = OFF,
+        STATISTICS_NORECOMPUTE = OFF,
+        IGNORE_DUP_KEY = OFF,
+        ALLOW_ROW_LOCKS =
+    ON,
+        ALLOW_PAGE_LOCKS =
+    ON
+    )
+ON  [PRIMARY]
 )
+ON  [PRIMARY]
