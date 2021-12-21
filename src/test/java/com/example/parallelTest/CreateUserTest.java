fd8e70a61a066b85d870904c1035ac4d3e877396
kannna5296 <kannnahacks5296@gmail.com>
2021-12-21 22:47:00 +0900

    DBアサート追加

--- src/test/java/com/example/parallelTest/CreateUserTest.java
+++ src/test/java/com/example/parallelTest/CreateUserTest.java
@@ -1,13 +1,17 @@
 package com.example.parallelTest;
 
+import static org.assertj.db.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import com.example.parallelTest.request.CreateUserRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.db.type.Source;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.assertj.db.type.Table;
+import org.assertj.db.type.Changes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -21,6 +25,8 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.sql.DataSource;
+import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,6 +40,8 @@ class CreateUserTest {
 
 	private MockMvc mockMvc;
 
+	private static Source dataSource;
+
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
@@ -46,6 +54,8 @@ class CreateUserTest {
 	@Test
 	void 登録なしの場合() throws Exception {
 
+		dataSource = new Source("jdbc:sqlserver://localhost:1433;databaseName=test;loginTimeout=30;socketTimeout=30000","sa", "passWord567");
+
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext) // (4)
 				.alwaysDo(log()).build();
 
@@ -55,9 +65,22 @@ class CreateUserTest {
 		request.setName("John");
 		request.setPassword("password");
 
+		Table table = new Table(dataSource,"app_user");
+		Changes changes = new Changes(table).setStartPointNow();
+
 		ResultActions results = mockMvc.perform(post("/user").content(mapper.writeValueAsString(request)).contentType(MediaType.APPLICATION_JSON_VALUE))
 				.andExpect(status().isOk());
 
+		changes.setEndPointNow();
+
+		assertThat(changes)
+				.ofCreation()
+				.change()
+				.rowAtEndPoint()
+				.value("name")
+				.isEqualTo("John")
+				.value("password")
+				.isEqualTo("password");
 	}
 
 }
