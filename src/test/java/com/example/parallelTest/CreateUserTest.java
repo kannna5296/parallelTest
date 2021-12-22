9569ee1715f9f5fed38d3c281050268c27b82e40
kannna5296 <kannnahacks5296@gmail.com>
2021-12-22 23:11:18 +0900

    Junit5対応・＠Before動くのを確認

--- src/test/java/com/example/parallelTest/CreateUserTest.java
+++ src/test/java/com/example/parallelTest/CreateUserTest.java
@@ -6,60 +6,45 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import com.example.parallelTest.request.CreateUserRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.db.type.Source;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.assertj.db.type.Table;
 import org.assertj.db.type.Changes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import javax.sql.DataSource;
-import javax.transaction.Transactional;
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 class CreateUserTest {
 
 	private MockMvc mockMvc;
 
-	private static Source dataSource;
+	private static Source dataSource = new Source("jdbc:sqlserver://localhost:1433;databaseName=test;loginTimeout=30;socketTimeout=30000","sa", "passWord567");
 
-	@Autowired
 	private WebApplicationContext webApplicationContext;
 
-//	@Before
-//	public void setUp() {
-//		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext) // (4)
-//				.alwaysDo(log()).build();
-//	}
+	private ObjectMapper mapper = new ObjectMapper();
+
+	@Autowired
+	public void setWac (WebApplicationContext webApplicationContext) {
+		this.webApplicationContext = webApplicationContext;
+	}
+
+	@BeforeEach
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext) // (4)
+				.alwaysDo(log()).build();
+	}
 
 	@Test
 	void 登録なしの場合() throws Exception {
-
-		dataSource = new Source("jdbc:sqlserver://localhost:1433;databaseName=test;loginTimeout=30;socketTimeout=30000","sa", "passWord567");
-
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext) // (4)
-				.alwaysDo(log()).build();
-
-		ObjectMapper mapper = new ObjectMapper();
 
 		CreateUserRequest request = new CreateUserRequest();
 		request.setName("John");
