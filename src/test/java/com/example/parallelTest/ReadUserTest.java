2040b5c38d4608f415637fc19fc8b06b1ca6d1f6
kannna5296 <kannnahacks5296@gmail.com>
2022-01-16 18:06:10 +0900

    Read試験追加

--- src/test/java/com/example/parallelTest/ReadUserTest.java
+++ src/test/java/com/example/parallelTest/ReadUserTest.java
@@ -1,8 +1,7 @@
 package com.example.parallelTest;
 
 import static com.github.springtestdbunit.annotation.DatabaseOperation.CLEAN_INSERT;
-import static org.assertj.db.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,18 +9,15 @@ import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 
-import com.example.parallelTest.request.CreateUserRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import com.github.springtestdbunit.operation.MicrosoftSqlDatabaseOperationLookup;
-import org.assertj.db.type.Changes;
 import org.assertj.db.type.Source;
-import org.assertj.db.type.Table;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.jdbc.SqlScriptsTestExecutionListener;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
@@ -43,8 +39,7 @@ import org.springframework.web.context.WebApplicationContext;
         DirtiesContextTestExecutionListener.class,
         TransactionDbUnitTestExecutionListener.class
 })
-class CreateUserTest {
-
+class ReadUserTest {
 
   private MockMvc mockMvc;
 
@@ -72,67 +67,28 @@ class CreateUserTest {
   }
 
   @Test
-  void 名前が同一のデータが存在しない場合() throws Exception {
-
-    CreateUserRequest request = new CreateUserRequest();
-    request.setName("John");
-    request.setPassword("password");
-
-    Table table = new Table(dataSource, "app_user");
-    Changes changes = new Changes(table).setStartPointNow();
-
-    ResultActions results =
-        mockMvc
-            .perform(
-                post("/user")
-                    .content(mapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andDo(log());
-
-    changes.setEndPointNow();
-
-    assertThat(changes)
-        .ofCreation()
-        .change()
-        .rowAtEndPoint()
-        .value("name")
-        .isEqualTo("John")
-        .value("password")
-        .isEqualTo("password");
-  }
-
-  @Test
   @DatabaseSetup(
           value = {
                   "/data/ユーザ作成/app_user.xlsx"
           },
           type = CLEAN_INSERT
   )
-  void 名前が同一のデータが存在する場合() throws Exception {
-
-    CreateUserRequest request = new CreateUserRequest();
-    request.setName("John");
-    request.setPassword("password");
-
-    Table table = new Table(dataSource, "app_user");
-    Changes changes = new Changes(table).setStartPointNow();
+  void データ1件() throws Exception {
 
     ResultActions results =
             mockMvc
                     .perform(
-                            post("/user")
-                                    .content(mapper.writeValueAsString(request))
-                                    .contentType(MediaType.APPLICATION_JSON_VALUE))
-                    .andExpect(status().isBadRequest())
+                            get("/user"))
+                    .andExpect(status().isOk())
                     .andDo(log());
 
-    changes.setEndPointNow();
+    String responseString = results.andReturn().getResponse().getContentAsString();
+    JSONObject result = new JSONObject(responseString);
 
     //DBに変更がないこと
-    assertThat(changes).ofCreation().hasNumberOfChanges(0);
-    assertThat(changes).ofModification().hasNumberOfChanges(0);
-    assertThat(changes).ofDeletion().hasNumberOfChanges(0);
+    //assertThat(changes).ofCreation().hasNumberOfChanges(0);
+    //assertThat(changes).ofModification().hasNumberOfChanges(0);
+    //assertThat(changes).ofDeletion().hasNumberOfChanges(0);
 
 
   }
